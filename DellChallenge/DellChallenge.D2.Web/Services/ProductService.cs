646964702e0862988bf589d1616abd54d0ec98f8
Luca Gabi <gabiluca@gmail.com>
2019-05-19 18:03:59 +0300

    code clean

--- DellChallenge/DellChallenge.D2.Web/Services/ProductService.cs
+++ DellChallenge/DellChallenge.D2.Web/Services/ProductService.cs
@@ -7,7 +7,7 @@ namespace DellChallenge.D2.Web.Services
     public class ProductService : IProductService
     {
         private RestClient apiClient;
-        private string action = "products";
+        private const string action = "products";
 
         public ProductService()
         {
@@ -16,28 +16,20 @@ namespace DellChallenge.D2.Web.Services
 
         private IRestResponse<TResult> apiCall<TResult>(Method method, string action, object body = null, Dictionary<string, object> parametters = null) where TResult : new()
         {
-            
             var apiRequest = new RestRequest(action, method, body==null? DataFormat.None:DataFormat.Json);
             if (parametters?.Count > 0)
-            {
                 foreach (var p in parametters)
-                {
                     apiRequest.AddParameter(p.Key, p.Value);
-                }
-            }
+
             if (body != null)
-            {
                 apiRequest.AddJsonBody(body);
-            }
             
             try
             {
                 var apiResponse = apiClient.Execute<TResult>(apiRequest);
                 if ((int)apiResponse.StatusCode / 100 != 2)
-                {
-                    //log...
-                    return apiResponse;
-                }
+                    ;//log...
+
                 return apiResponse;
             }
             catch (System.Exception)
@@ -49,33 +41,25 @@ namespace DellChallenge.D2.Web.Services
 
         public ProductModel Add(NewProductModel newProduct)
         {
-
             var apiResponse = apiCall<ProductModel>(Method.POST, action, newProduct);
-
             return apiResponse.Data;
         }
 
         public IEnumerable<ProductModel> GetAll()
         {
-
             var apiResponse = apiCall<List<ProductModel>>(Method.GET,action);
-
             return apiResponse.Data;
         }
 
         public ProductModel Get(string id)
         {
-
             var apiResponse = apiCall<ProductModel>(Method.GET, action+"/"+id);
-
             return apiResponse.Data;
         }
 
         public ProductModel Update(ProductModel product)
         {
-            
             var apiResponse = apiCall<ProductModel>(Method.PUT,action,product);
-
             return apiResponse.Data;
         }
 
